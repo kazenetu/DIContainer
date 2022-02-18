e7e714ff527797c4a2b326318328108e930bf44d
kazenetu <rakudafactory@gmail.com>
2022-02-18 22:25:52 +0900

    コンソールプロジェクトでLib/TinyDIContainerを利用する #3

--- DIContainer/Program.cs
+++ DIContainer/Program.cs
@@ -8,19 +8,19 @@ namespace DIContainer
     static void Main(string[] args)
     {
       // DIコンテナ登録
-      DIContainer.Add<ITest1, Test1>();
-      DIContainer.Add<ITest2, Test2>();
+      TinyDIContainer.DIContainer.Add<ITest1, Test1>();
+      TinyDIContainer.DIContainer.Add<ITest2, Test2>();
 
 
       // 利用例
       // Test1のインスタンス生成
-      var test1 = DIContainer.CreateInstance<ITest1>();
+      var test1 = TinyDIContainer.DIContainer.CreateInstance<ITest1>();
 
       Console.WriteLine($"Test1.Test() [{test1.Test()}]");
       Console.WriteLine($"Test1.GetName() [{test1.GetName()}]");
 
       // Test2のインスタンス生成
-      var test2 = DIContainer.CreateInstance<ITest2>();
+      var test2 = TinyDIContainer.DIContainer.CreateInstance<ITest2>();
 
       Console.WriteLine($"Test2.GetTest() [{test2.GetTest()}]");
       Console.WriteLine($"Test2.GetName() [{test2.GetName()}]");
